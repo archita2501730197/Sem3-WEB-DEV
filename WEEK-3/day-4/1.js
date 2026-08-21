552aa107f498b62bd9b9beba375d290fcdf025d6
Archita <archita@Archita--MacBook-Air.local>
2026-08-21 12:18:33 +0530

    updated

--- WEEK-3/day-4/1.js
+++ WEEK-3/day-4/1.js
@@ -1,87 +1,96 @@
-// step -1
+// step-1
+const { log } = require("console");
 const express = require("express");
 const fs = require("fs");
-
-// Step -2
+// step-2
 const app = express();
-
-// middleware  --> req.body --> parse
+// middleware --> req.body --> parse
 app.use(express.json());
 
-// step -3 Routes/Api
+// step-3 Routes/Api
 app.get("/", (req, res) => {
-  res.send({ msg: "welcome to express server" });
+  res.send({ msg: "welcome to exprrss server" });
 });
 
 app.get("/home", (req, res) => {
-  res.send({ msg: "welcome to home Page" });
+  res.send({ msg: "welcome to home page" });
 });
 
-// GET API/ Read Route
 app.get("/read", (req, res) => {
   const data = fs.readFileSync("./1.json", "utf-8");
-  // console.log(data, typeof data);
+  // console.log(data, typeod data);
 
-  // converting json data ---- parse ----> js object
+  // converting json data --- parse ---> js object
   const jsData = JSON.parse(data);
-  console.log(jsData, typeof jsData);
+  console.log(jsData, typeof jsdata);
 
-  console.log(jsData.student);
+  console.log(jsData);
 
   res.send({ data: jsData });
 });
-
-// GET API/ Read Route only for student
-app.get("/stread", (req, res) => {
-  const data = fs.readFileSync("./1.json", "utf-8");
-  // console.log(data, typeof data);
-
-  // converting json data ---- parse ----> js object
-  const jsData = JSON.parse(data);
-  console.log(jsData, typeof jsData);
-
-  console.log(jsData.student);
-
-  res.send({ data: jsData.student });
-});
-
-// GET API/ Read Route only for trainer
-app.get("/trread", (req, res) => {
-  const data = fs.readFileSync("./1.json", "utf-8");
-  // console.log(data, typeof data);
-
-  // converting json data ---- parse ----> js object
-  const jsData = JSON.parse(data);
-  console.log(jsData, typeof jsData);
-
-  console.log(jsData.trainer);
-
-  res.send({ data: jsData.trainer });
-});
-
 app.post("/create", (req, res) => {
   const payload = req.body;
-  console.log(payload); // {}
+  console.log(payload);
+  const data = JSON.parse(fs.readFileSync("./1.json", "utf-8"));
 
-  const data = JSON.parse(fs.readFileSync("./1.json", "utf-8"));  // {}
-
-  const stdata = data.student; // []
+  const stdata = data.student;
   console.log(stdata);
   stdata.push(payload);
   console.log(stdata);
 
-  data.student = stdata;
+// partially updating the value thorugh spread operator (...)
+  app.put("/create", (req, res) => {
+  const payload = req.body;
+  console.log(payload);
+  const data = JSON.parse(fs.readFileSync("./1.json", "utf-8"));
 
-  fs.writeFileSync("./1.json", JSON.stringify(data));  // overwrite (stringify converts object to java)
+  const stdata = data.student;
+  console.log(stdata);
+  stdata.push(payload);
+  console.log(stdata);
 
-  res.send({ msg: "New student Created successfully" });
+    const updatedData = stdata.map((el) => {
+    if (el.id == req.params.id) {
+      return {...el, ...payload};
+    };
+
+  fs.writeFileSync("./1.json", JSON.stringify(data));
+  res.send({ msg: "new student added successfully" });
+
+  res.send({ msg: "New student created successfully" });
 });
+  });
+});
+app.patch("/update/:id", (req, res) => {
+  const payload = req.body;
+
+  const id = req.params;
+  console.log(id);
+
+  const data = JSON.parse(fs.readFileSync("./1.json", "utf-8"));
+
+  const stdata = data.student;
+  console.log(stdata);
+
+  const updatedData = stdata.map((el) => {
+    if (el.id == req.params.id) {
+      return payload;
+    } else return el;
+  });
+  console.log(updatedData);
+  data.student = updatedData;
+  fs.writeFileSync("./1.json", JSON.stringify(data));
+
+  res.send({ msg: "student record updated" });
+});
+
 app.listen(8080, () => {
   console.log("Server started");
 });
 
-// only 3 methods client talks to server 
-// 1. query parameter    
-// 2. body parameter     req.body
-// 3. req.params       /:id
+// post ---> add, create krna
 
+// client 3 tarike se client server se baat chit krta hai  (req bhejta hai)
+// 1. query parameter (most popular ) [www.google.com/?color=black& size=32]
+// body parameter (req.body)
+// req.params (/:id)[react router dom mein padha tha dynamic routing mein ]
